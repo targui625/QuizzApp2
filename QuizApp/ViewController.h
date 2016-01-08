83b29a16e889ae25db0718ea3a622c201b27950d
高木則迪 <dc667_tg@yahoo.co.jp>
2016-01-08 12:52:46 +0900

    Initial Commit

---
+++ QuizApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuizApp
+//
+//  Created by Norimichi Takagi on 2016/01/08.
+//  Copyright © 2016年 NorimichiTakagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QuizApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuizApp
+//
+//  Created by Norimichi Takagi on 2016/01/08.
+//  Copyright © 2016年 NorimichiTakagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
